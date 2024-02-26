f7a0b974c4b9631d3baa8a95bd0a36dd813c5c9d
v3less11 <159018254+v3less11@users.noreply.github.com>
2024-02-26 15:00:41 +0300

    Add files via upload

---
+++ func5.cpp
@@ -0,0 +1,20 @@
+#include "easy.h"
+
+bool itc_ispositive(int num){
+    if(num >= 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+
+
+}
+bool itc_ispositive_d(double num){
+    if(num >= 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
