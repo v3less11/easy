4117461f6a9fcf2b7b359e3ac329a9b8e03bdae4
v3less11 <159018254+v3less11@users.noreply.github.com>
2024-03-11 04:45:45 -0700

    Add files via upload

---
+++ func2.cpp
@@ -0,0 +1,51 @@
+int itc_revnbr(int num){
+    int res = 0;
+    
+    if(num < 0){
+        int num2 = -num;
+        while (num2 > 0){
+            res *= 10;
+            res += num2%10;
+            num2/=10;
+        }
+        return -res;
+    }
+    while (num > 0){
+        res *= 10;
+        res += num%10;
+        num/=10;
+    }
+    return res;
+}
+
+
+bool itc_iseven(int num)
+{
+    if(num % 2 == 0)
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+int itc_max(int num, int num2)
+{
+    if(num > num2){
+        return num;
+    }
+    else{
+        return num2;
+    }
+}
+int itc_min(int min1, int min2)
+{
+    if(min1 < min2)
+    {
+        return min1;
+    }
+    else
+    {
+        return min2;
+    }
+}
---
+++ func3.cpp
@@ -0,0 +1,55 @@
+double itc_fmax(double num, double num2)
+{
+    if (num > num2)
+    {
+        return num;
+    }
+    else
+    {
+        return num2;
+    }
+}
+
+double itc_fmin(double num, double num2)
+{
+    if (num < num2)
+    {
+        return num;
+    }
+    else
+    {
+        return num2;
+    }
+}
+int itc_sqrt(int num)
+{
+    
+    int i = 0;
+    if(num <= 0){
+        return -1;
+    }
+    else{
+        while(i*i < num){
+            i++;
+        }
+        if(i*i == num){
+            return i;
+        }
+        else{
+            return i-1;
+        }
+    }
+}
+int itc_skv(int num)
+{
+    if (num <= 0)
+    {
+        return -1;
+    }
+    else
+    {
+        int tmp = num * num;
+        return tmp;
+    }
+}
+
